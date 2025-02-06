068a0e8d4606b6253be2225925a69c3c00353d54
glopesdev <glopesdev@users.noreply.github.com>
2025-02-06 10:00:22 +0000

    Add regression test for missing part names

--- tests/io/test_reader.py
+++ tests/io/test_reader.py
@@ -27,9 +27,16 @@ def test_Pose_read_local_model_dir_with_register_prefix():
 
 @pytest.mark.api
 def test_Pose_read_local_model_dir_with_missing_class_vectors():
-    """Test that the Pose reader raises KeyError when model directory does not contain class vectors."""
+    """Test that the Pose reader raises KeyError when config file does not contain class vectors."""
     with pytest.raises(KeyError):
         aeon.load(pose_path / "missing-class-vectors", social03.CameraTop.Pose)
+
+
+@pytest.mark.api
+def test_Pose_read_local_model_dir_with_missing_part_names():
+    """Test that the Pose reader raises KeyError when config file does not contain part names."""
+    with pytest.raises(KeyError):
+        aeon.load(pose_path / "missing-part-names", social03.CameraTop.Pose)
 
 
 if __name__ == "__main__":

0c192931943445e500257dfd0e8eb58fe48e5344
glopesdev <glopesdev@users.noreply.github.com>
2025-02-06 09:51:51 +0000

    Add regression test for missing class vectors

--- tests/io/test_reader.py
+++ tests/io/test_reader.py
@@ -8,20 +8,28 @@ from swc import aeon
 from tests.schema import social02, social03
 
 pose_path = Path(__file__).parent.parent / "data" / "pose"
+topdown_path = pose_path / "topdown"
 
 
 @pytest.mark.api
 def test_Pose_read_local_model_dir():
-    """Test that the Pose stream can read a local model directory."""
-    data = aeon.load(pose_path, social02.CameraTop.Pose)
+    """Test that the Pose reader can read a local model directory."""
+    data = aeon.load(topdown_path, social02.CameraTop.Pose)
     assert len(data) > 0
 
 
 @pytest.mark.api
 def test_Pose_read_local_model_dir_with_register_prefix():
-    """Test that the Pose stream can read a local model directory with a register prefix."""
-    data = aeon.load(pose_path, social03.CameraTop.Pose)
+    """Test that the Pose reader can read a local model directory with a register prefix."""
+    data = aeon.load(topdown_path, social03.CameraTop.Pose)
     assert len(data) > 0
+
+
+@pytest.mark.api
+def test_Pose_read_local_model_dir_with_missing_class_vectors():
+    """Test that the Pose reader raises KeyError when model directory does not contain class vectors."""
+    with pytest.raises(KeyError):
+        aeon.load(pose_path / "missing-class-vectors", social03.CameraTop.Pose)
 
 
 if __name__ == "__main__":

1f11f9c79f9f6e8a95abd0b201890dcc1497e87d
lochhh <changhuan.lo@ucl.ac.uk>
2025-06-30 15:01:09 +0100

    Add warning message match for nonmonotonic data test

--- tests/io/test_api.py
+++ tests/io/test_api.py
@@ -54,7 +54,7 @@ def test_pose_load_nonmonotonic_data():
 
 @pytest.mark.api
 def test_pose_load_nonmonotonic_data_time_start_only_sort_fallback():
-    with pytest.warns(UserWarning):
+    with pytest.warns(UserWarning, match="data index for .* contains out-of-order timestamps!"):
         data = aeon.load(
             nonmonotonic_path, social03.CameraTop.Pose, start=pd.Timestamp("2024-07-03T10:00:00")
         )

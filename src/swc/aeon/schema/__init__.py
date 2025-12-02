8eca632f2bd2f37152044ea799c1458e29a0874d
glopesdev <g.lopes@neurogears.org>
2025-12-02 14:59:22 +0000

    Rename dataset model and allow nested readers
    This allows composing groups of data readers into the experiment while
    retaining the same inferred container prefix.
    
    Refactoring of decorator and model validation allows future changes to
    inference rules.

--- src/swc/aeon/schema/__init__.py
+++ src/swc/aeon/schema/__init__.py
@@ -1,6 +1,6 @@
 """Modules for building aeon schemas."""
 
 # Set imports available directly under 'swc.aeon.schema'
-from swc.aeon.schema.base import BaseSchema, DataSchema, Device, Experiment, Metadata, data_reader
+from swc.aeon.schema.base import BaseSchema, Dataset, Device, Experiment, Metadata, data_reader
 
-__all__ = ["BaseSchema", "Experiment", "Device", "DataSchema", "Metadata", "data_reader"]
+__all__ = ["BaseSchema", "Experiment", "Device", "Dataset", "Metadata", "data_reader"]

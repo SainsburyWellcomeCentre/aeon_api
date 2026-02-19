3240703cfe4110bd6bdad2879fd4bd968d0d0334
glopesdev <g.lopes@neurogears.org>
2026-02-19 21:28:34 +0000

    Remove device discriminator class (#69)
    Avoid depending on a common property as an implicit discriminator.
    
    For polymorphic designs, named discriminator types can be
    added as required.
    
    The base Device class was removed as it was left empty. BaseSchema
    should be used instead.

--- src/swc/aeon/schema/__init__.py
+++ src/swc/aeon/schema/__init__.py
@@ -1,6 +1,6 @@
 """Modules for building aeon schemas."""
 
 # Set imports available directly under 'swc.aeon.schema'
-from swc.aeon.schema.base import BaseSchema, Dataset, Device, Experiment, Metadata, data_reader
+from swc.aeon.schema.base import BaseSchema, Dataset, Experiment, Metadata, data_reader
 
-__all__ = ["BaseSchema", "Experiment", "Device", "Dataset", "Metadata", "data_reader"]
+__all__ = ["BaseSchema", "Experiment", "Dataset", "Metadata", "data_reader"]
